b751a9e06bf4babed97241d055fdc179e64d1af1
Frankie Guapo <frankie.guapo@gmail.com>
2023-08-03 13:01:34 -0500

    Breakup loading Darwin Index

--- print_all_darwin_files.py
+++ print_all_darwin_files.py
@@ -12,9 +12,11 @@ def print_darwin_index_folders_list():
     # Fit formatting of loader call
     parent_path = "/darwin_indices/"
     folder_index = 1
+    print('[')
     for folder in all_index_folders:
-        print(f'storage_folder{folder_index} = "{parent_path}{folder}"')
+        print(f'"{parent_path}{folder}",')
         folder_index += 1
+    print(']')
 
 def create_storage_folder_shorthand_list(end_index, start_index=1):
     for ind in range(start_index, end_index + 1):
@@ -33,8 +35,8 @@ def print_chunk_list(end_index, start_index=1):
         print(f"\"chunk{ind}\",")
 
 if __name__ == "__main__":
-    #print_darwin_index_folders_list()
+    print_darwin_index_folders_list()
     #create_storage_folder_shorthand_list(end_index=778, start_index=1)
     #create_load_index_list(end_index=778, start_index=1)
     #print_index_shorthands_list(end_index=778, start_index=1)
-    print_chunk_list(end_index=778, start_index=1)
+    #print_chunk_list(end_index=778, start_index=1)
